34d0bb48cbf1b8fb8f69e2b5c14d812cd917fa26
wswanbeck <wswanbeck@silversky.com>
2013-09-21 10:36:16 -0400

    expand the config to include actions when things are found (email is the only action for now)

--- lib/config.py
+++ lib/config.py
@@ -1,7 +1,7 @@
 import json
 import pdb
 
-class Watch:
+class Alert:
     # watchtype = ""
     def a(self):
         pass
@@ -22,18 +22,26 @@ class Config:
         
         # read in the alerts
         alerts = cfg_json["alerts"]
-        for watch in alerts:
-            newwatch = Watch()
-            newwatch.watchtype = watch["type"]
+        for alert in alerts:
+            # parse the watch seciton of the alert
+            watch = alert["watch"]
+            newalert = Alert()
+            newalert.watchtype = watch["type"]
             schedule = watch["schedule"]
-            newwatch.scheduletype = schedule["type"]
-            newwatch.scheduledays = []
+            newalert.scheduletype = schedule["type"]
+            newalert.scheduledays = []
             for day in schedule["days"]:
-                newwatch.scheduledays.append(day)
-            newwatch.trip = watch["trip"]
-            newwatch.scheduled_time = watch["scheduled-time"]
-            newwatch.watch_feed_name = watch["watch-feed-name"]
-            self.watches[watch["alertname"]] = newwatch
+                newalert.scheduledays.append(day)
+            newalert.trip = watch["trip"]
+            newalert.scheduled_time = watch["scheduled-time"]
+            newalert.watch_feed_name = watch["watch-feed-name"]
+
+            # parse the action section of the alert
+            action = alert["action"]
+            newalert.actiontype = action["type"]
+            if newalert.actiontype == "email":
+                newalert.actionemailaddress = action["emailaddress"]
+            self.watches[alert["alertname"]] = newalert
         
         # read in the feeds
         for f in cfg_json["feeds"]:
