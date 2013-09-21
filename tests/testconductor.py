fe88a000e6daf6de85e6f3b87c3615c0178ad9f1
wswanbeck <wswanbeck@silversky.com>
2013-09-20 21:07:58 -0400

    start working on the conductor (REST server)

--- tests/testconductor.py
+++ tests/testconductor.py
@@ -5,36 +5,25 @@ import unittest
 import os
 import pdb
 import sys
-import time
 # pretend we're running from the directory above ./tests
 sys.path.append(os.path.abspath('..'))
 from lib.config import Config
-from lib.feedwatcher import FeedWatcher
+
+import bottle
+from bottle import route, run
+
+@route('/', method='GET')
+def homepage():
+    return 'Hello World'
+
+@route('/alert/:alertname', method='POST')
+def post_alert(alertname):
+    return dict(name='what to say now...')
 
 class TestConductor(unittest.TestCase):
 
-    def testStartWatchers(self):
-        cfg = Config("../conf/test.conf")
-        feedwatchers = []
-        feedwatchers.append(FeedWatcher(cfg.feeds[0].feedurl))
-        feedwatchers.append(FeedWatcher(cfg.feeds[1].feedurl))
-        for fw in feedwatchers:
-            fw.start()
-            time.sleep(1)
-        time.sleep(2)
-        for alertname in cfg.watches:
-            feedwatchers[0].subscribe("http://localhost:88/alert?alertname=" + alertname, "myname")
-            time.sleep(1)
-        time.sleep(12)
-        
-        for alertname in cfg.watches:
-            feedwatchers[0].unsubscribe("http://localhost:88/alert?alertname=" + alertname)
-
-        time.sleep(12)
-        for fw in feedwatchers:
-            fw.signalstop()
+    def test1(self):
+        run(host='localhost', port=88)
 
 if __name__ == '__main__':
     unittest.main()
-
-
